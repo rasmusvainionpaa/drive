3872a92107099de2666b8d336b64aff40c774c90
Rasmus Vainionpää <46357949+RasmusVainionpaa@users.noreply.github.com>
2022-12-21 22:23:25 +0200

    Update [...nextauth].ts
    Changed authentication provider from discord to credentials

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -19,12 +19,45 @@ export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: 'Credentials',
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: {  label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+  
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user
+        } else {
+          // If you return null or false then the credentials will be rejected
+          return null
+          // You can also Reject this callback with an Error or with a URL:
+          // throw new Error('error message') // Redirect to error page
+          // throw '/path/to/redirect'        // Redirect to a URL
+        }
+      }
+    })
     // ...add more providers here
   ],
 };
 
 export default NextAuth(authOptions);
+function CredentialsProvider(arg0: {
+  // The name to display on the sign in form (e.g. 'Sign in with...')
+  name: string;
+  // The credentials is used to generate a suitable form on the sign in page.
+  // You can specify whatever fields you are expecting to be submitted.
+  // e.g. domain, username, password, 2FA token, etc.
+  credentials: { username: { label: string; type: string; placeholder: string; }; password: { label: string; type: string; }; }; authorize(credentials: any, req: any): Promise<{ id: number; name: string; email: string; } | null>;
+}): import("next-auth/providers/index.js").Provider {
+  throw new Error("Function not implemented.");
+}
+
