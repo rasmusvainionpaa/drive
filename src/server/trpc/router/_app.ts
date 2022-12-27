b26e6c2a971200b13af87653a63cc0f3101f3a06
Rasmus Vainionpää <46357949+RasmusVainionpaa@users.noreply.github.com>
2022-12-27 14:30:19 +0200

    Update _app.ts
    added new route for trpc

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,13 @@
+
 import { router } from "../trpc";
 import { authRouter } from "./auth";
 import { exampleRouter } from "./example";
+import { UserRouter } from "./createUser";
 
 export const appRouter = router({
   example: exampleRouter,
   auth: authRouter,
+  user: UserRouter,
 });
 
 // export type definition of API
