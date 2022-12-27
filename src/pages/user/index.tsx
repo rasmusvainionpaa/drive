c1335d2bed671a2cb796436494eeaf8aa6c39e6a
Rasmus Vainionpää <46357949+RasmusVainionpaa@users.noreply.github.com>
2022-12-27 14:30:04 +0200

    Create index.tsx
    users profile page

---
+++ src/pages/user/index.tsx
@@ -0,0 +1,39 @@
+import { type NextPage } from "next";
+import { getSession, useSession } from "next-auth/react";
+import Layout from "../../components/Layout";
+
+export const getServerSideProps = async (context: any) => {
+  const session = await getSession(context);
+  
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth/signin",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
+};
+
+const User: NextPage = () => {
+  const { data: session, status } = useSession();
+
+  console.log(session);
+
+  if (status === "loading") {
+    return <main>Loading...</main>;
+  }
+
+  return (
+    <Layout>
+        <p>User profile</p>
+      <p>name: {session?.user?.name}</p>
+    </Layout>
+  );
+};
+
+export default User;
