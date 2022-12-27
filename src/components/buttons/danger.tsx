9e691dfb7e394a6eafefed8c4afb8f31d9bbc447
Rasmus Vainionpää <46357949+RasmusVainionpaa@users.noreply.github.com>
2022-12-27 14:28:40 +0200

    Create danger.tsx

---
+++ src/components/buttons/danger.tsx
@@ -0,0 +1,8 @@
+export default function DangerButton() {
+  return (
+    <button
+      className="px-4 py-2 font-bold text-white bg-red-600 rounded-md hover:bg-red-700"
+    >
+    </button>
+  );
+}
