bf0f33f1ec67f0eaef44914981cd56054b58ef20
Rasmus Vainionpää <46357949+RasmusVainionpaa@users.noreply.github.com>
2022-12-30 14:34:14 +0200

    Create [...directory].tsx

---
+++ src/pages/folders/[...directory].tsx
@@ -0,0 +1,54 @@
+import { type NextPage } from "next";
+import { getSession, useSession } from "next-auth/react";
+import getFilesWebDav from "src/utils/webdav/getFilesWebDav";
+import Layout from "../../components/Layout";
+import File from "../../components/File";
+import {FileStat} from "webdav";
+import Link from "next/link";
+
+interface Props {
+  files: FileStat[];
+}
+
+export const getServerSideProps = async (context: any) => {
+
+    const directory = context.query.directory;
+
+    const url = Object.values(directory).join("/");
+
+    const rawFiles = await getFilesWebDav("/" + url)
+    
+    const files = JSON.parse(JSON.stringify(rawFiles))
+
+    return {
+        props: { 
+            files: files,
+        },
+    };
+};
+
+const Files: NextPage<Props> = (files) => {
+
+    return (
+        <Layout>
+            <div className="mb-5 flex flex-row text-3xl">
+                <h1 className="ml-2">
+                    <Link href="/">Files / </Link>
+                </h1>
+            </div>   
+            <div className="mb-5">
+                <ul className="grid grid-cols-3 gap-4">
+                    {
+                        files.files.map((file: any) => {
+                            return (
+                                <File key={file.etag} file={file} />
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+        </Layout>
+    );
+};
+
+export default Files;
