f188cd031096334f6a5218356b1935484dabae98
Rasmus Vainionpää <46357949+RasmusVainionpaa@users.noreply.github.com>
2022-12-31 14:39:02 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,35 +2,12 @@ import { type NextPage } from "next";
 import { getSession, useSession } from "next-auth/react";
 import Layout from "../components/Layout";
 
-export const getServerSideProps = async (context: any) => {
-  const session = await getSession(context);
-  
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/files",
-        permanent: false,
-      },
-    };
-  }
-
-  return {
-    props: { session },
-  };
-};
 
 const Home: NextPage = () => {
-  const { data: session, status } = useSession();
-
-  console.log(session);
-
-  if (status === "loading") {
-    return <main>Loading...</main>;
-  }
 
   return (
     <Layout>
-      <p>Hello {session?.user?.name}</p>
+      <p>Hello from home</p>
     </Layout>
   );
 };
