707bf6a639ad2afe31849ef200599abfaf9be1d5
Rasmus Vainionpää <46357949+RasmusVainionpaa@users.noreply.github.com>
2023-01-02 00:06:40 +0200

    Update [directory].tsx

--- src/pages/f/[directory].tsx
+++ src/pages/f/[directory].tsx
@@ -7,46 +7,50 @@ import Link from "next/link";
 import base64 from "base-64";
 import FilePath from "src/components/FilePath";
 import { useRouter } from "next/router";
-import { FileStat } from "webdav";
+import { FileType } from "src/types/fileType";
 
 interface Props {
-  files: FileStat[];
+  files: FileType[];
+  currentUrl: string;
 }
 
-export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const url =
+    context.query.directory === undefined
+      ? ""
+      : base64.decode(context.query.directory as string);
 
-    const url = context.query.directory === undefined ? "" : base64.decode(context.query.directory as string);
+  console.log("yeet: ", url);
+  const files = await getFilesWebDav(url);
 
-    const files = await getFilesWebDav(url)
-
-    return {
-        props: { 
-            files: files,
-        },
-    };
+  return {
+    props: {
+      files: files,
+      currentUrl: url,
+    },
+  };
 };
 
-const Files: NextPage<Props> = (files) => {
+const Files: NextPage<Props> = (files, currentUrl) => {
+  console.log(currentUrl);
 
-    return (
-        <Layout>
-            <div className="flex justify-start text-xl">
-                <FilePath />
-            </div>
+  return (
+    <Layout>
+      <div className="flex justify-start text-xl">
+        <FilePath />
+      </div>
 
-            <div className="py-2">
-                <ul className="grid grid-row">
-                    {
-                        files.files.map((file: FileStat) => {
-                            return (
-                                <File key={file.etag} file={file} />
-                            )
-                        })
-                    }
-                </ul>
-            </div>
-        </Layout>
-    );
+      <div className="py-2">
+        <ul className="grid-row grid">
+          {files.files.map((file: FileType) => {
+            return <File key={file.etag} file={file} />;
+          })}
+        </ul>
+      </div>
+    </Layout>
+  );
 };
 
 export default Files;
