86fd0bdcc34c3ca72dd7cd049bc1b1d48175ca20
Rasmus Vainionpää <46357949+RasmusVainionpaa@users.noreply.github.com>
2023-01-02 00:06:45 +0200

    Create fileType.d.ts

---
+++ src/types/fileType.d.ts
@@ -0,0 +1,11 @@
+export interface FileType {
+  filename: string;
+  basename: string;
+  lastmod: string;
+  size: number;
+  type: "file" | "directory";
+  etag: string | null;
+  mime?: string;
+  props?: DAVResultResponseProps;
+  //url?: string;
+}
