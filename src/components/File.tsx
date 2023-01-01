1a73535ae6d2f33fca964096587657ccf7d21fc9
Rasmus Vainionpää <46357949+RasmusVainionpaa@users.noreply.github.com>
2023-01-02 00:06:37 +0200

    Update File.tsx

--- src/components/File.tsx
+++ src/components/File.tsx
@@ -1,55 +1,51 @@
 import Link from "next/link";
 import base64 from "base-64";
 import { useRouter } from "next/router";
+import { FileType } from "src/types/fileType";
 import { FileStat } from "webdav";
 
 interface Props {
-    file: FileStat;
+  file: FileStat;
 }
 
-export default function File({file}: Props) {
-    const router = useRouter()
-    const directory = router.query.directory === undefined ? "" : base64.decode(router.query.directory as string);
+export default function File({ file }: Props) {
+  const router = useRouter();
+  const directory =
+    router.query.directory === undefined
+      ? ""
+      : base64.decode(router.query.directory as string);
 
-    const url = base64.encode(`${directory}/${file.basename}`);
+  const url = base64.encode(`${directory}/${file.basename}`);
 
-    if (file.type === "directory") {
-        return (
-            <li className="w-full">
-                <Link href={"/f/" + url}>
-                    <div className="flex flex-row justify-between">
-                        <div className="flex flex-row">
-                            <img src="/assets/folder-s.svg" alt="folder" />
-                            <p className="flex justify-start px-4">
-                                {file.basename}
-                            </p>
-                        </div>
-                        <div className="flex flex-row">
-                            <p className="px-4">
-                                {file.lastmod}
-                            </p>
-                        </div>
-                    </div>
-                </Link>
-            </li>
-        );
-    }
-
+  if (file.type === "directory") {
     return (
-        <li className="w-full">
-            <div className="flex flex-row justify-between">
-                <div className="flex flex-row">
-                    <img src="/assets/file.svg" alt="file" />
-                    <p className="flex justify-start px-4">
-                        {file.basename}
-                    </p>
-                </div>
-                <div className="flex flex-row"> 
-                    <p className="px-4">
-                        {file.lastmod}
-                    </p>
-                </div>
+      <li className="w-full">
+        <Link href={"/f/" + url}>
+          <div className="flex flex-row justify-between">
+            <div className="flex flex-row">
+              <img src="/assets/folder-s.svg" alt="folder" />
+              <p className="flex justify-start px-4">{file.basename}</p>
             </div>
-        </li>
+            <div className="flex flex-row">
+              <p className="px-4">{file.lastmod}</p>
+            </div>
+          </div>
+        </Link>
+      </li>
     );
+  }
+
+  return (
+    <li className="w-full">
+      <div className="flex flex-row justify-between">
+        <div className="flex flex-row">
+          <img src="/assets/file.svg" alt="file" />
+          <p className="flex justify-start px-4">{file.basename}</p>
+        </div>
+        <div className="flex flex-row">
+          <p className="px-4">{file.lastmod}</p>
+        </div>
+      </div>
+    </li>
+  );
 }
