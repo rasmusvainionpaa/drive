d3bc1d12435068c0f3acfc3fb5b3d5794bb746c2
Rasmus Vainionpää <46357949+RasmusVainionpaa@users.noreply.github.com>
2023-03-18 10:54:30 +0200

    Update Directory.tsx

--- src/components/Directory.tsx
+++ src/components/Directory.tsx
@@ -1,22 +1,22 @@
+import { Item } from "@prisma/client";
 import Link from "next/link";
-import { FileType } from "src/types/fileType";
 
 interface Props {
-  file: FileType;
+  file: Item;
 }
 
 export default function Directory({ file }: Props) {
 
     return (
         <li className="w-full">
-            <Link href={file.url!}>
+            <Link href={"/f/" + file.base64path}>
                 <div className="flex flex-row justify-between">
                     <div className="flex flex-row">
                         <img src="/assets/folder-s.svg" alt="folder" />
-                        <p className="flex justify-start px-4">{file.basename}</p>
+                        <p className="flex justify-start px-4">{file.name}</p>
                     </div>
                     <div className="flex flex-row">
-                        <p className="px-4">{file.lastmod}</p>
+                        <p className="px-4">{file.updatedAt as unknown as string}</p>
                     </div>
                 </div>
             </Link>
