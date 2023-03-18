0be046d515838b590143eb2ba877d36e0f1806bd
Rasmus Vainionpää <46357949+RasmusVainionpaa@users.noreply.github.com>
2023-03-18 10:54:57 +0200

    Update [[...directory]].tsx

--- src/pages/f/[[...directory]].tsx
+++ src/pages/f/[[...directory]].tsx
@@ -1,74 +1,159 @@
-import { GetServerSidePropsContext, type NextPage } from "next";
-import { getSession, useSession } from "next-auth/react";
-import getFilesWebDav from "src/utils/webdav/getFilesWebDav";
 import Layout from "../../components/Layout";
-import File from "../../components/File";
-import Link from "next/link";
-import base64 from "base-64";
-import FilePath from "src/components/FilePath";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { GetServerSidePropsContext, NextPage } from "next/types";
+import { getSession, useSession } from "next-auth/react";
+import { prisma } from "../../server/db/client";
+import FileComponent from "src/components/FileComponent";
+import { Item } from "@prisma/client";
 import { useRouter } from "next/router";
-import { FileType } from "src/types/fileType";
 import Directory from "src/components/Directory";
-import createUrlArray from "src/utils/createUrlArray";
 import sortByFileType from "src/utils/sortByFileType";
-import Toolbar from "src/components/Toolbar";
+import createUrlArray from "src/utils/createUrlArray";
+import base64 from "base-64";
 
 interface Props {
-  files: FileType[];
+  currUrl: string
+  Files: Item[]
 }
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+  const session = await getSession(context)
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth/signin",
+        permanent: false,
+      },
+    };
+  }
+
+  const id = session?.user.id as string
 
   const url =
     context.query.directory === undefined
       ? "/"
-      : base64.decode(context.query.directory as string);
+      : base64.decode(context.query.directory as string)
 
+  const urlArray = createUrlArray(url)
 
+  console.log("Url array: ", urlArray)
 
-  const files = await getFilesWebDav(url);
+  console.log("Current url: ", url)
+      
+  try {
+    const files = await prisma.user.findMany({
+      where: {
+        id: id
+      },
+      select: {
+        items: {
+          where: {
+            folderPath: url as string
+          }
+        }
+      }
+    })
 
-  return {
-    props: {
-      files: files,
-    },
-  };
+    return {
+        props: {
+          currUrl: url as string,
+          Files: sortByFileType(JSON.parse(JSON.stringify(files[0]?.items))),
+        },
+    };
+  } catch(e) {
+    console.log(e)
+    return {
+      props: {
+        currUrl: (url + "/"),
+      },
+    };
+  }
 };
 
-const Files: NextPage<Props> = (files) => {
+const Files: NextPage<Props> = ({currUrl, Files}) => {
+  const [file, setFile] = useState<any>(null)
+  const router = useRouter()
 
-  const router = useRouter();
-  const url = router.query.directory === undefined ? ["/"] : base64.decode(router.query.directory as string).split("/");
+  const uploadFile = async () => {
 
-  const finishedArray = createUrlArray(url)
-  const filesSortedArray = sortByFileType(files.files);
-  
+    // retreave pre-signed url from server
+    let { data } = await axios.post("/api/s3/upload", {
+      name: file.name,
+      type: file.type,
+      folder: currUrl,
+    });
+
+    // parse pre-signed url
+    const url = data.url;
+
+    // send data to bucket with pre-signed url
+    await axios.put(url, file, {
+      headers: {
+        "Content-type": file.type,
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Header": "*",
+      },
+    });
+    setFile(null);
+    router.reload()
+  };
+
+  const handleSubmit = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault()
+    if(event.target.files) {
+      setFile(event.target.files[0])
+    }
+    event.target.files = null
+  }
+
+  const createFolder = async () => {
+    let newFolder = prompt("Folder name")
+
+    if(newFolder == null || newFolder == "") {
+      return
+    }
+
+    await axios.post("/api/files/folder", {
+      folderName: newFolder,
+      folderPath: currUrl,
+    });
+    router.reload()
+  }
+ 
+  useEffect(() => {
+    if (file) {
+      const uploadedFileDetail = async () => await uploadFile();
+      uploadedFileDetail();
+    }
+  }, [file]);
+
   return (
     <Layout>
-      <Toolbar />
-      <div className="flex justify-start text-l">
-        <div className="justify-between border-b border-blue-500">
-          <ul className="flex flex-row">
-            {finishedArray.map((url, index) => {
-              return <FilePath key={index} path={url.url} name={url.name}/>;
-              }
-            )}
-          </ul>
-        </div>
+      <div>
+        <button onClick={() => history.back()}>Go Back</button>
       </div>
-
-      <div className="pb-2 pt-4">
-        <ul className="grid-row grid">
-          {filesSortedArray.map((file: FileType) => {  
-            if(file.type === "directory") {
-              file.url = `/f/${base64.encode(file.filename)}`;
-              return <Directory key={file.etag} file={file} />;
-            }
-            file.url = `/show/${base64.encode(file.filename)}`;
-            return <File key={file.etag} file={file} />;
-          })}
+      <div>
+        <input
+          type="file"
+          accept="image/*"
+          name="image"
+          id="selectFile"
+          onChange={(e: any) => handleSubmit(e)}
+        />
+        <button onClick={() => createFolder()}>Create folder</button>
+      </div>
+      <div>
+        <ul>
+        {Files && Files.map((file: Item) => {
+          if(file.isFolder) {
+            return <Directory key={file.id} file={file} />
+          }
+          return (
+            <FileComponent key={file.name} file={file} />
+          )
+        })}
         </ul>
       </div>
     </Layout>
