1503134f166ff8ff8213134fc2c92a741913f846
Rasmus Vainionpää <46357949+RasmusVainionpaa@users.noreply.github.com>
2023-03-18 10:55:18 +0200

    Update sortByFileType.ts

--- src/utils/sortByFileType.ts
+++ src/utils/sortByFileType.ts
@@ -1,16 +1,16 @@
-import { FileType } from "src/types/fileType";
+import { Item } from "@prisma/client";
 
 /**
  * Sorts an array of files by type.
  * @param files array of files
  * @returns sorted array of files
  */
-export default function sortByFileType(files: FileType[]): FileType[] {
+export default function sortByFileType(files: Item[]): Item[] {
 
     const sortedFiles = files.sort((a, b) => {
-        if (a.type === "directory" && b.type === "file") {
+        if (a.isFolder) {
             return -1;
-        } else if (a.type === "file" && b.type === "directory") {
+        } else if (b.isFolder) {
             return 1;
         } else {
             return 0;
